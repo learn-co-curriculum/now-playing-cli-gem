91be175296ab158937000d8f7ee92cc1eaf47f1a
aviflombaum <avi@flombaum.com>
2015-11-13 20:41:04 -0500

    almost

--- lib/now_playing.rb
+++ lib/now_playing.rb
@@ -1,9 +1,45 @@
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
+
 require "now_playing/version"
+require "now_playing/movie"
 
 module NowPlaying
   class CLI
     def call
-      puts "Now playing"
+
+      start
+    end
+
+    def list
+      puts "Now Playing in Theatres\n"
+      Movie.all.each.with_index(1) do |movie, i|
+        puts "#{i}. #{movie.name}"
+      end
+    end
+
+    def start
+      list
+      input = nil
+      while input != "exit"
+        puts "What movie would you more information on, by name or number?"
+        puts "Enter list to see the movies again."
+        puts "Enter exit to end the program."
+        input = gets.strip
+        if input == "list"
+          list
+        elsif input.to_i == 0
+          if movie = Movie.find_by_name(input)
+            puts movie.url
+          end
+        elsif input.to_i > 0
+          if movie = Movie.find(input.to_i)
+            puts movie.url
+          end
+        end
+      end
+      puts "Goodbye!!!"
     end
   end
 end
